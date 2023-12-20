464cc746fa1e040694780127378d0cd6aa367c4c
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-20 12:04:54 +0630

    Adding Task Done features in the taskify project.

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faPenToSquare, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faPenToSquare,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
 import { Todo } from "../model";
-import './singleTodo.css'
+import "./singleTodo.css";
+import TodoList from "./TodoList";
 
 type Props = {
   todo: Todo;
@@ -11,15 +16,32 @@ type Props = {
 };
 
 function SingleTodo({ todo, todos, setTodo }: Props) {
+  const handleDone = (id: number) => {
+    setTodo(
+      todos.map((todo) =>
+        todo.id == id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
     <form className="todos_single">
-      <span className="todos_single_text">{todo.todo}</span>
-      <div>
+      {todo.isDone ? (
+        <s className="todos_single_text">{todo.todo}</s>
+      ) : (
+        <span className="todos_single_text">{todo.todo}</span>
+      )}
 
-        <span className="icon"><FontAwesomeIcon icon={faPenToSquare} /></span>
-        <span className="icon"><FontAwesomeIcon icon={faTrash} /></span>
-        <span className="icon"><FontAwesomeIcon icon={faCheck} /></span>
-        
+      <div>
+        <span className="icon">
+          <FontAwesomeIcon icon={faPenToSquare} />
+        </span>
+        <span className="icon">
+          <FontAwesomeIcon icon={faTrash} />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <FontAwesomeIcon icon={faCheck} />
+        </span>
       </div>
     </form>
   );
