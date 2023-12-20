5ed9e82ba5ef985e19e5b58c76bde901972cb8d3
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-20 11:13:27 +0630

    Adding Fontawsome icon and try to complete the SingleTodo tsx file.

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPenToSquare, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { Todo } from "../model";
+import './singleTodo.css'
 
-function SingleTodo() {
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodo: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+function SingleTodo({ todo, todos, setTodo }: Props) {
   return (
-    <div>SingleTodo</div>
-  )
+    <form className="todos_single">
+      <span className="todos_single_text">{todo.todo}</span>
+      <div>
+
+        <span className="icon"><FontAwesomeIcon icon={faPenToSquare} /></span>
+        <span className="icon"><FontAwesomeIcon icon={faTrash} /></span>
+        <span className="icon"><FontAwesomeIcon icon={faCheck} /></span>
+        
+      </div>
+    </form>
+  );
 }
 
-export default SingleTodo
+export default SingleTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Todo } from '../model'
+import React from "react";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 
-interface Props{
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-export default function TodoList({todos, setTodos}: Props) {
+export default function TodoList({ todos, setTodos }: Props) {
   return (
-      <div className='todos'>
-          {todos.map((todo => {
-              return <>
-                  <li>{ todo.todo}</li>
-              </>
-          }))}
+    <div className="todos">
+      {todos.map((todo) => {
+        return (
+          <>
+            <SingleTodo
+              todo={todo}
+              key={todo.id}
+              todos={todos}
+              setTodo={setTodos}
+            />
+          </>
+        );
+      })}
     </div>
-  )
+  );
 }
