2d9ac27e933d6f5592620c2c6579c178a0380172
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-22 13:10:22 +0630

    Adding Feature to be available to do drag and drop tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./model";
 import TodoList from "./components/TodoList";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 function App() {
   const [todo, setTodo] = useState<string>("");
@@ -19,9 +19,42 @@ function App() {
     }
   };
 
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    let add,
+      active = todos,
+      complete = CompletedTodos;
+    
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add)
+    } else {
+      complete.splice(destination.index, 0, add)
+    }
+
+    setCompletedTodos(complete)
+    setTodos(active)
+
+  };
+
   return (
     <React.StrictMode>
-      <DragDropContext onDragEnd={() => {}}>
+      <DragDropContext onDragEnd={onDragEnd}>
         <div className="App">
           <span className="heading">Taskify</span>
           <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
